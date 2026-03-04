5e3f57d6e0d8e9a8e9e17e02c02fa09dbb8a9628
Ruy Schmidt <11483561+Ruy-Bala@users.noreply.github.com>
2026-03-03 21:39:33 -0300

    Atualizar o mysqrt.c

--- mysqrt.c
+++ mysqrt.c
@@ -1,3 +1,17 @@
-int mysqrt(int a) {
-	return ++a;
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ int mysqrt (int);
+ 
+ int main (int argc, char *argv[]) {
+      int a = 0;
+      int A = 0;
+      int X = 0;
+
+     X = atoi(argv[1]);
+
+     a = mysqrt(X);
+     A = a * a;
+     printf(":%d :%d\n", a, A);
+     return a;
 }
