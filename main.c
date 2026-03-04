37ade71b7d12f0e81365871447d163112b907eac
Ruy Schmidt <11483561+Ruy-Bala@users.noreply.github.com>
2026-03-03 21:50:12 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -1,31 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ int mysqrt (int);
+ 
+ int main (int argc, char *argv[]) {
+     int a = 0;
+     int A = 0;
+     int X = 0;
 
-int mysqrt (int);
+     X = atoi(argv[1]);
 
-int main (int argc, char *argv[]) {
-	int a = 0;
-	int X = 0;
-
-	X = atoi(argv[1]);
-
-	a = mysqrt(X);
-	printf(":%d\n", a);
-	return a;
-}
-
-int mysqrt (int X){
-	int a = 0, A = 0;
-
-	A = a * a;
-	
-	while (A <= X){
-		a++;
-		A = a * a;
-	};
-
-	a--;
-	A = a * a;
-	
-	return a;
+     a = mysqrt(X);
+     A = a * a;
+     printf(":%d :%d\n", a, A);
+     return a;
 }
